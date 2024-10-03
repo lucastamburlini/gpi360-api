c7cf916df360bb2e64c3d14d0392f042fdd4b5ea
lucastamburlini <lucastamburlini@gmail.com>
2024-10-03 09:10:05 -0300

    probando

--- dist/src/utils/urlValidator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = urlValidator;
-function urlValidator(value) {
-    if (value && !/^https?:\/\/[^\s$.?#].[^\s]*$/i.test(value)) {
-        throw new Error("You must provide a valid URL.");
-    }
-}
